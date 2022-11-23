cd0ae11de45c47b936a1977ef93da509dd4d0fda
Martin D'Agostino <martin.dagostino@klarna.com>
2022-11-23 16:25:18 +0100

    debugging

--- api/nasa.go
+++ api/nasa.go
@@ -11,15 +11,15 @@ import (
 func Nasa(w http.ResponseWriter, r *http.Request) {
 
 	log.Printf("Request with UserAgent %s and Path %s\n", r.UserAgent(), r.URL.Path)
-	
+
 	if r.Method != "GET" {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
 
-	if r.URL.Path == "/api/nasa/topics" || strings.HasPrefix(r.URL.Path, "/api/nasa/topics/") {
+	if true || r.URL.Path == "/api/nasa/topics" || strings.HasPrefix(r.URL.Path, "/api/nasa/topics/") {
 		feedUrls := config.GetUrls()
 
-		if r.URL.Path == "/api/nasa/topics" || r.URL.Path == "/api/nasa/topics/" {
+		if true || r.URL.Path == "/api/nasa/topics" || r.URL.Path == "/api/nasa/topics/" {
 			nasa.GetTopics(feedUrls, w)
 		} else {
 			topic := strings.Split(r.URL.Path, "/")[4]
